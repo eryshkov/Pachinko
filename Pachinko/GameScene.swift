235dc0c5f66a2f49698acfef0ad406b4705e9e59
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-12 23:53:05 +0300

    create a box

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -125,6 +125,16 @@ class GameScene: SKScene {
             }else{
                 if editingMode {
                     //create a box
+                    let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                    let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                    box.zRotation = CGFloat.random(in: 0...3)
+                    box.position = location
+                    
+                    box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                    box.physicsBody?.isDynamic = false
+                    
+                    addChild(box)
+                    
                 }else{
                     //create a ball
                     let ball = SKSpriteNode(imageNamed: "ballRed")
