c01358a8619029a8998c7b94f6a8f06b1ce84a7b
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-12 20:34:55 +0300

    didMove changed assets added

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -12,10 +12,14 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        <#code#>
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        <#code#>
+        
     }
 }
